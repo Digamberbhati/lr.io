db6a6824df5ce083f307b5a36a20c0c66eb3de2d
Bablumahto <bablumht18@gmail.com>
2024-11-11 11:15:04 +0530

    added code

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,16 @@
 
 import { Route, Routes } from "react-router-dom"
 import Navbar from "./components/Navbar"
-import Footer from './components/footer'
+import Footer from './components/Footer'
 import Home from "./pages/home"
 import About from "./components/About"
+import Clients from "./components/Clients"
+import Diet from "./components/Diet"
+import Training from "./components/Training"
+import Contact from "./components/Contact"
+
+
+
 
 
 function App() {
@@ -17,6 +24,10 @@ function App() {
     <Routes>
       <Route path="/" element={<Home/>}  />
       <Route path="/about" element={<About/>}  />
+      <Route path="/clients" element={<Clients/>}  />
+      <Route path="/diet" element={<Diet/>}  />
+      <Route path="/training" element={<Training/>}  />
+      <Route path="/contact" element={<Contact/>}  />
     </Routes>
 
   <Footer/>
