a05332f2b13148eb1b84ee0b57665ba666d83fab
Bablumahto <bablumht18@gmail.com>
2024-11-11 15:50:58 +0530

    whychoose

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,7 +5,7 @@ function home() {
     <>
     <main >
     {/* Home Section */}
-    <section className="hero relative bg-cover bg-center bg-[url('public/images/lokesh1.jpg')]" id="home">
+    <section className="hero relative bg-cover bg-center bg-[url('public/images/personal/IMG_2.jpg')] md:bg-[url('public/images/lokesh3.jpg')]" id="home">
       <div className="absolute"></div>
       <div className="relative mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
         <div className="max-w-xl text-center pt-20 sm:pt-0 bottom-0 sm:text-left">
@@ -35,57 +35,75 @@ function home() {
     </section>
   </main>
 
-  {/* <!-- Counter --> */}
+{/* why choose us */}
 
-<div class="counter-wrapper">
-    <div class="counter  flex flex-col items-center">
-       
-            <img src="images/svg/arrrow.svg" alt="" class="h-10"/>
-            <div class="text-[#1D4ED8] font-bold flex items-center ">
-                <h1 class="count " data-target="469">0</h1>
-                <span class="text-[3vw]">K+</span>
-            </div>
-          
-       
-        <p>Social media Followers</p>
-    </div>
-   
-    <div class="counter  flex flex-col items-center">
-       
-        <img src="images/svg/awwards.svg" alt="" class="h-10 "/>
-        <div class="text-[#1D4ED8] font-bold flex items-center ">
-            <h1 class="count " data-target="400">0</h1>
-            {/* <!-- <span class="text-[3vw]">K+</span> --> */}
+<section className="about text-textdark px-8 pt-9 pb-6" id="about">
+  <div className="container mx-auto min-h-screen">
+    <h1 className="sm:text-[5vw] text-[3vh] text-center pt-4 text-black font-black tracking-tighter uppercase">
+      Why Choose us
+    </h1>
+    <h2 className="sm:text-[2vw] text-center font-black text-blue-700 uppercase">
+      because we provide top Notch training
+    </h2>
+
+    {/* <!-- cards --> */}
+    <div className="wrapper sm:max-w-screen-xl mx-auto mt-8 w-full flex flex-col items-center gap-8 sm:flex-row">
+      {/* <!-- Personal Training --> */}
+      <div className="card relative hover:info-visible">
+        <img src="images/personal/IMG_7.jpg" alt="" loading="lazy" className="filter drop-shadow-xl w-full h-full object-cover"/>
+        {/* <!-- Gradient overlay --> */}
+        <div className="gradient-overlay absolute inset-0"></div>
+        <div className="info absolute inset-0  flex-col justify-center items-start text-white z-10 hidden">
+          <h1>Personal Training</h1>
+          <p>Transform your fitness journey with personalized gym training! Achieve your goals faster with tailored workouts, expert guidance, and unwavering motivation. Start your path to success today!</p>
+          <a href="join.html" className="btn">Join Now</a>
         </div>
-      
-   
-    <p>Awards Won</p>
-</div>
+        <h1 className="absolute bottom-4 uppercase font-black sm:text-[2vw] text-[4vh] text-zinc-200">Personal Training</h1>
+      </div>
 
-<div class="counter  flex flex-col items-center">
-       
-    <img src="images/svg/people.svg" alt="" class="h-10"/>
-    <div class="text-[#1D4ED8] font-bold flex items-center ">
-        <h1 class="count " data-target="550">0</h1>
-        <span class="text-[3vw]">+</span>
+      {/* <!-- Home Workout --> */}
+      <div className="card relative hover:info-visible">
+        <img src="images/personal/IMG_3.jpg" alt="" loading="lazy" className="filter drop-shadow-xl w-full h-full object-cover"/>
+        {/* <!-- Gradient overlay --> */}
+        <div className="gradient-overlay absolute inset-0"></div>
+        <div className="info absolute inset-0 flex flex-col justify-center items-start text-white z-10 hidden">
+          <h1>Home Workout</h1>
+          <p>Maximize your home workout with these tips: use bodyweight exercises, create a routine, stay consistent, and mix cardio with strength training for balanced results.</p>
+          <a href="join.html" className="btn">Join Now</a>
+        </div>
+        <h1 className="absolute bottom-4 uppercase font-black sm:text-[2vw] text-[4vh] text-zinc-200">Home Workout</h1>
+      </div>
+
+      {/* <!-- Weight Lifting --> */}
+      <div className="card relative hover:info-visible">
+        <img src="images/personal/IMG_2.jpg" alt="" loading="lazy" className="filter drop-shadow-xl w-full h-full object-cover"/>
+        {/* <!-- Gradient overlay --> */}
+        <div className="gradient-overlay absolute inset-0"></div>
+        <div className="info absolute inset-0  flex-col justify-center items-start text-white z-10 hidden">
+          <h1>Weight Lifting</h1>
+          <p>
+            Weight lifting builds strength, improves muscle tone, and boosts metabolism. Incorporate various exercises and proper form to enhance overall fitness and achieve your goals effectively.
+          </p>
+          <a href="join.html" className="btn">Join Now</a>
+        </div>
+        <h1 className="absolute bottom-4 uppercase font-black sm:text-[2vw] text-[4vh] text-zinc-200">Weight Lifting</h1>
+      </div>
+
+      {/* <!-- Arm Wrestling --> */}
+      <div className="card relative hover:info-visible">
+        <img src="images/personal/IMG_4.jpg" alt="" loading="lazy" className="filter drop-shadow-xl w-full h-full object-cover"/>
+        {/* <!-- Gradient overlay --> */}
+        <div className="gradient-overlay absolute inset-0"></div>
+        <div className="info absolute inset-0  flex-col justify-center items-start text-white z-10 hidden">
+          <h1>Arm Wrestling</h1>
+          <p>Arm wrestling tests strength and strategy in a head-to-head battle. With grit and technique, competitors push their limits to claim victory.</p>
+          <a href="join.html" className="btn">Join Now</a>
+        </div>
+        <h1 className="absolute bottom-4 uppercase font-black sm:text-[2vw] text-[4vh] text-zinc-200">Arm Wrestling</h1>
+      </div>
     </div>
-  
-
-<p>Clients</p>
-</div>
-
-<div class="counter  flex flex-col items-center">
-       
-    <img src="images/svg/collab.svg" alt="" class="h-10"/>
-    <div class="text-[#1D4ED8] font-bold flex items-center ">
-        <h1 class="count " data-target="1000">0</h1>
-        <span class="text-[3vw]">+</span>
-    </div>
-  
-
-<p>Brands Collabration</p>
-</div>
-</div>
+  </div>
+</section>
   </>
       
   )
